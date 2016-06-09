af10e7a7cdcbcf928d3b9480b73b254191cbeef2
BladeLordLyndis <shu0018x@outlook.com>
2016-06-09 00:14:16 -0400

    Problem 1

--- ProjectEuler/src/ProjectEuler/Problem1.java
+++ ProjectEuler/src/ProjectEuler/Problem1.java
@@ -1,5 +1,32 @@
 package ProjectEuler;
 
-public class Problem1 {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+public class Problem1 {
+  public static void main(String[] args) {
+    HashMap<Integer, Boolean> multiples = new HashMap<Integer, Boolean>();
+    int multiple = 1;
+    
+    while (multiple * 3 < 1000) {
+      multiples.put(multiple * 3, true);
+      multiple++;
+    }
+    
+    multiple = 1;
+    while (multiple * 5 < 1000) {
+      multiples.put(multiple * 5, true);
+      multiple++;
+    }
+  
+    int sum = 0;
+    Iterator it = multiples.entrySet().iterator();
+    while (it.hasNext()) {
+      Map.Entry pair = (Map.Entry)it.next();
+      sum += (int) pair.getKey();
+    }
+    
+    System.out.println(sum);
+  }
 }
